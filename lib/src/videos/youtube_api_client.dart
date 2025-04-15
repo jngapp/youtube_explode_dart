f45c6470cbe13f523516419445c8e22e7fa8b4f5
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2025-04-15 01:51:42 -1000

    Update youtube_api_client.dart

--- lib/src/videos/youtube_api_client.dart
+++ lib/src/videos/youtube_api_client.dart
@@ -36,7 +36,20 @@ class YoutubeApiClient {
       }
     },
   }, 'https://www.youtube.com/youtubei/v1/player?key=AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc&prettyPrint=false');
-
+  static const customAndroidClient = YoutubeApiClient({
+    'context': {
+      'client': {
+        'clientName': 'ANDROID',
+        'clientVersion': '19.09.37',
+        'androidSdkVersion': 30,
+        'userAgent':
+        'com.google.android.youtube/19.09.37 (Linux; U; Android 11) gzip',
+        'hl': 'en',
+        'timeZone': 'UTC',
+        'utcOffsetMinutes': 0,
+      },
+    },
+  }, 'https://www.youtube.com/youtubei/v1/player?key=AIzaSyA8eiZmM1FaDVjRy-df2KTyQ_vz_yYM39w&prettyPrint=false');
   /// This provides also muxed streams but seems less reliable than [ios].
   /// If you require an android client use [androidVr] instead.
   static const android = YoutubeApiClient({
